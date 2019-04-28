7544021ebb17313895087fe8183c1fde8d98cbdd
Precocious <47267836+Precocious@users.noreply.github.com>
2019-04-28 20:37:57 +0200

    first commit
    Initial commit of module 4 assignment.

---
+++ ma4/script.js
@@ -0,0 +1,10 @@
+var btn = document.getElementById('btn_success');
+
+btn.addEventListener('click', function(e){
+    alert('Success');
+    // e.stopPropagation();
+})
+
+function btnSucc() {
+    alert('SUCCESS');
+}
